9c513516bd025559377c885217054866833c8dd3
Jung-woo Lee <you@example.com>
2021-11-16 17:20:11 +0900

    time change

--- clinicalInformation/batch_scheduler/batchUpdater.py
+++ clinicalInformation/batch_scheduler/batchUpdater.py
@@ -3,6 +3,6 @@ from clinicalInformation.views import BatchDataView
 def start():
   scheduler = BackgroundScheduler()
   batch = BatchDataView()
-  scheduler.add_job(batch.post, "interval", minutes=1,id="batch_001",replace_existing=True)
+  scheduler.add_job(batch.post, "interval", minutes=60,id="batch_001",replace_existing=True)
   scheduler.start()
 
